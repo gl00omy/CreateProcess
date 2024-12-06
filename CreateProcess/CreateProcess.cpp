1739c8f3e1c1ca6a18b85a0021674c9257c629b0
Zhibek Tyulemissova <zhibektyulemissova@gmail.com>
2024-12-06 06:03:18 -0800

    WaitForSingleObject: a function that waits for the Notepad process to terminate or time out after a specified period.

--- CreateProcess/CreateProcess.cpp
+++ CreateProcess/CreateProcess.cpp
@@ -11,26 +11,24 @@ int main()
    // memset(&si, 0, sizeof(si));
    // si.cb = sizeof(si);
     PROCESS_INFORMATION pi;
-    BOOL success = CreateProcessW(nullptr, name, nullptr, nullptr, FALSE,
-        HIGH_PRIORITY_CLASS, nullptr, nullptr, &si, &pi);
+    BOOL success = CreateProcessW(nullptr, name, nullptr, nullptr, FALSE, HIGH_PRIORITY_CLASS, nullptr, nullptr, &si, &pi);
     if (success) {
         printf("PID: %u TID: %u\n", pi.dwProcessId, pi.dwThreadId);
-
+        if (WAIT_TIMEOUT == WaitForSingleObject(pi.hProcess, 10000)) {
+            printf("Notepad is still running ... \n");
+        }
+        else {
+        DWORD code;
+        GetExitCodeProcess(pi.hProcess, &code);
+        printf("Notepad has terminated with code: %u\n", code);
+        }
+       
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
     }
+
     else {
         printf("Error creating process: %u\n", GetLastError());
     }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
